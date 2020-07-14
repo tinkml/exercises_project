118ad3c2c999bc9ae81ab2699263f679fe642305
tinkml <1996ntishenko@gmail.com>
2020-07-14 22:30:55 +0500

    Fixed project's libraries imports

--- exercises/views.py
+++ exercises/views.py
@@ -1,9 +1,7 @@
 import pyperclip
-from django.db import IntegrityError
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
-from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth import logout
 
 from .models import Category, UsersCategoryLevel
 from .task_handlers import *
